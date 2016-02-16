dc4f9d016330b4c1b05a8003add2e819d75a2658
Vinh Dang <dtt.vinh@gmail.com>
2016-02-16 11:49:05 +0700

    Add get user info route.

--- server/app/routes/users.js
+++ server/app/routes/users.js
@@ -3,6 +3,9 @@
 const router = require('koa-router')();
 const auth = require('../passport_auth');
 
+const db = require('../db');
+const entities = require('../entities');
+
 module.exports = router;
 
 router.put('add user', '/', (ctx, next) => {
@@ -10,7 +13,13 @@ router.put('add user', '/', (ctx, next) => {
     ctx.body = { user: ctx.request.body };
 }).get('get user', '/:username', auth.authenticate(['user:read']), ctx => {
     console.log('render body');
-    ctx.body = { user: { username: ctx.params.username } };
+    return db.users.findByEmail(ctx.params.username).exec().then(user => {
+        if (!user) {
+            ctx.body = { user: null, error: 'Invalid user' };
+        } else {
+            ctx.body = { user: entities.userReference(user) };
+        }
+    });
 }).post('update user', '/:username', auth.authenticate(['user:update']), ctx => {
     let user_info = ctx.params.user;
     if (ctx.params.username !== 'me') {
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -3,6 +3,9 @@
 const router = require('koa-router')();
 const auth = require('../passport_auth');
 
+const db = require('../db');
+const entities = require('../entities');
+
 module.exports = router;
 
 router
@@ -14,7 +17,13 @@ router
         auth.authenticate(['user:read']),
         ctx => {
             console.log('render body');
-            ctx.body = {user: {username: ctx.params.username}};
+            return db.users.findByEmail(ctx.params.username).exec().then(user => {
+                if (!user) {
+                    ctx.body = {user: null, error: 'Invalid user'};
+                } else {
+                    ctx.body = {user: entities.userReference(user)};
+                }
+            });
         }
     )
     .post('update user', '/:username',
