802e5598952ea885f6117a33c76d2adc8c6159a1
Vinh Dang <dtt.vinh@gmail.com>
2016-02-17 11:34:03 +0700

    Update user route to accept empty params.

--- server/app/routes/users.js
+++ server/app/routes/users.js
@@ -46,19 +46,19 @@ router.put('add user', '/', function (ctx, next) {
     }
     var postData = ctx.request.body.user,
         user = ctx.state.user;
-    if (postData.gender) {
-        user.gender = postData.gender.toLowerCase();
+    if (postData.gender !== undefined) {
+        user.gender = postData.gender ? postData.gender.toLowerCase() : 'na';
     }
-    if (postData.firstName) {
+    if (postData.firstName !== undefined) {
         user.firstName = postData.firstName;
     }
-    if (postData.lastName) {
+    if (postData.lastName !== undefined) {
         user.lastName = postData.lastName;
     }
-    if (postData.dob) {
+    if (postData.dob !== undefined) {
         user.dob = new Date(postData.dob);
     }
-    if (postData.phone) {
+    if (postData.phone !== undefined) {
         user.phone = postData.phone;
     }
     if (postData.picture && postData.picture.url) {
