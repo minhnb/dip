e10326476e6a5e8358bbbaebd51a751d4729f5c1
James Kenaley <james.kenaley1@gmail.com>
2016-02-16 20:42:14 -0700

    added dotting

---
+++ server/src/config/index.js
@@ -0,0 +1,14 @@
+var environment = process.env.NODE_ENV || 'development';
+environment = environment.toLowerCase();
+
+module.exports = {
+    env: environment,
+    mongo: {
+        uri: process.env.URI
+    },
+    jwt: {
+        key: process.env.JWT_KEY,
+        algorithm: process.env.JWT_ALG,
+        issuer: 'dip'
+    }
+};
