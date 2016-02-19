89212de6517c694a24b00d45413e662052ee31ac
James Kenaley <james.kenaley1@gmail.com>
2016-02-18 21:06:53 -0700

    added hip chat logger

--- server/app/app.js
+++ server/app/app.js
@@ -7,6 +7,8 @@ var convert = require('koa-convert');
 var logger = require('koa-logger');
 var dotenv = require('dotenv');
 
+var Hipchatter = require('hipchatter');
+
 var co = require('co');
 var json = require('koa-json');
 //const onerror = require('koa-onerror');
@@ -14,6 +16,8 @@ var error = require('koa-error');
 
 var bodyparser = require('koa-bodyparser')();
 var path = require('path');
+
+var hipchatter = new Hipchatter('4561f3fb57f873056b4eea66e7bf27');
 
 var rootFolder = path.normalize(__dirname + '/../..'),
     serverFolder = rootFolder + '/server';
@@ -47,6 +51,13 @@ app.use(function (ctx, next) {
 app.use(function (ctx, next) {
     return next().catch(function (err) {
         ctx.response.status = err.status || 400;
+        hipchatter.notify('Dip-DevOps', {
+            message: err.message,
+            color: 'yellow',
+            token: 'By0ccj1t2AEjdbhgCD7O3FjMhWllcJWb4xkaIBCs'
+        }, function (err) {
+            if (err == null) console.log('Successfully notified the room.');
+        });
         if (config.env != 'production' && config.env != 'prod') {
             console.log('caught', err);
             ctx.response.body = err.message || 'Bad Request';
@@ -61,6 +72,15 @@ app.use(function (ctx, next) {
 // This is clean and nice, but we couldn't modify it to our needs
 // Considering create a simple error-template for dev env and do it ourselves
 //app.use(convert(error()));
+process.on('uncaughtException', function (err) {
+    hipchatter.notify('Dip-DevOps', {
+        message: 'Error: ' + err + ' - Enviroment: ' + process.env.NODE_ENV,
+        color: 'red',
+        token: '3GitaVbKmdLiOle45NIzalVVEMJpPgOst1yEr8jH'
+    }, function (err) {
+        if (err == null) console.log("error", err, 'Successfully notified the team.');
+    });
+});
 
 app.on('error', function (err, ctx) {
     console.error('server error', err, ctx);
--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,8 @@ const convert = require('koa-convert');
 const logger = require('koa-logger');
 const dotenv = require('dotenv');
 
+const Hipchatter = require('hipchatter');
+
 const co = require('co');
 const json = require('koa-json');
 //const onerror = require('koa-onerror');
@@ -14,6 +16,8 @@ const error = require('koa-error');
 
 const bodyparser = require('koa-bodyparser')();
 const path = require('path');
+
+var hipchatter = new Hipchatter('4561f3fb57f873056b4eea66e7bf27');
 
 var rootFolder = path.normalize(__dirname + '/../..'),
     serverFolder = `${rootFolder}/server`;
@@ -47,6 +51,13 @@ app.use((ctx, next) => {
 app.use((ctx, next) => {
     return next().catch(err => {
         ctx.response.status = err.status || 400;
+        hipchatter.notify('Dip-DevOps',{
+            message: err.message,
+            color: 'yellow',
+            token: 'By0ccj1t2AEjdbhgCD7O3FjMhWllcJWb4xkaIBCs'
+        }, function(err){
+            if (err == null) console.log('Successfully notified the room.');
+        });
         if (config.env != 'production' && config.env != 'prod') {
             console.log('caught', err);
             ctx.response.body = err.message || 'Bad Request';
@@ -61,6 +72,15 @@ app.use((ctx, next) => {
 // This is clean and nice, but we couldn't modify it to our needs
 // Considering create a simple error-template for dev env and do it ourselves
 //app.use(convert(error()));
+process.on('uncaughtException', function(err) {
+    hipchatter.notify('Dip-DevOps',{
+        message: `Error: ${err} - Enviroment: ${process.env.NODE_ENV}`,
+        color: 'red',
+        token: '3GitaVbKmdLiOle45NIzalVVEMJpPgOst1yEr8jH'
+    }, function(err){
+        if (err == null) console.log("error", err, 'Successfully notified the team.');
+    });
+});
 
 app.on('error', function (err, ctx) {
     console.error('server error', err, ctx);
