1074ed7b6cc199a80e3a0b3798d9cfb6b13eafb4
Vinh Dang <dtt.vinh@gmail.com>
2016-03-27 17:15:49 +0700

    Add jsonfile package. Update populate-pools migration script.

--- server/src/migrations/1458887982056-populate_pools.js
+++ server/src/migrations/1458887982056-populate_pools.js
@@ -20,123 +20,117 @@ const geocoder = require('node-geocoder')(geocoderProvider, httpAdapter);
 //sleep(1000);
 const jsonfile = require('jsonfile');
 exports.up = function(next) {
-	let file_path = path.join(__dirname, `assets/pools.json`);
+    let file_path = path.join(__dirname, `assets/pools.json`);
     fs.readFile(file_path, 'utf8', function (err, data) {
-	    if (err) throw err;
-	    var pools = JSON.parse(data);
-      	continuing(pool => {
-      		pool.coordinates = [];
-      		pool.address = {
-      			street: '',
-      			city: '',
-      			state: '',
-      			postalCode: 0,
-      			country: 'US'
-      		}
-      		return geocoder.geocode(pool.fullAddress)
-      		.then(data => {
-      			pool.address.street = pool.fullAddress.split(",")[0];
-      			pool.address.city = pool.city;
-      			if(data.length > 0) {
-      				pool.address.state = data[0].administrativeLevels.level1short;
-      				pool.coordinates.push(data[0].longitude);
-					pool.coordinates.push(data[0].latitude);
-      			}
-      			
-      			let tmpAddArr = pool.fullAddress.split(" ");
-      			pool.address.postalCode = tmpAddArr[tmpAddArr.length -1];
-				return pool;
-      		});
-      	}, pools, 0, 1).then(pools => {
- 		    let imgPromises = pools.map(pool => {
- 		      	pool.tmpName = pool.name.replace(/ /g, "_").toLowerCase();
- 		      	let img_path = path.join(__dirname, `assets/pools/${pool.tmpName}.jpg`);
- 		      	if (fs.existsSync(img_path)) { 
- 					try {
- 						let data = fs.readFileSync(img_path);
- 						return s3.upload(`pools/${pool.tmpName}`, data, 'image/jpg').then(()=> {
- 						});
- 					} catch (err) {
- 						return Promise.reject(err);
- 					}
- 				}else {
- 					return Promise.resolve();
- 				}
- 			});
+        if (err) throw err;
+        var pools = JSON.parse(data);
+        continuing(pool => {
+            pool.coordinates = [];
+            pool.address = {
+                street: '',
+                city: '',
+                state: '',
+                postalCode: 0,
+                country: 'US'
+            };
+            return geocoder.geocode(pool.fullAddress)
+            .then(data => {
+                pool.address.street = pool.fullAddress.split(",")[0];
+                pool.address.city = pool.city;
+                if(data.length > 0) {
+                    pool.address.state = data[0].administrativeLevels.level1short;
+                    pool.coordinates.push(data[0].longitude);
+                    pool.coordinates.push(data[0].latitude);
+                }
 
- 			Promise.all(imgPromises).then(imgs => {
- 				//append img url
- 				for (let i = 0; i < pools.length; i++) {
- 					if(imgs[i] !== undefined) {
- 						pools[i].image = {
- 						    url: imgs[i].Location,
- 						    verified: true
- 						};
- 					}			
- 				}
- 				//append amenities
- 				pools.map(pool => {
- 					pool.amenities = [];
- 					let now = new Date();
- 					pool.createdAt = now,
- 					pool.updatedAt = now
- 					if(pool.chair != '') {
- 						pool.amenities.push({
- 							type: 'chair',
- 							count: pool.chair,
- 							createdAt: now,
- 							updatedAt: now
- 						})
- 					}
- 					if(pool.daybed != '') {
- 						pool.amenities.push({
- 							type: 'daybed',
- 							count: pool.daybed,
- 							createdAt: now,
- 							updatedAt: now
- 						})
- 					}
- 					if(pool.cabana != '') {
- 						pool.amenities.push({
- 							type: 'cabana',
- 							count: pool.cabana,
- 							createdAt: now,
- 							updatedAt: now
- 						})
- 					}
- 					delete pool.chair;
- 					delete pool.daybed;
- 					delete pool.cabana;
- 					delete pool.tmpName;
- 					delete pool.fullAddress;
- 				})
- 				return connectionPromise.then(connection => {
- 				    connection.db.collection('pools', (error, collection) => {
- 				        collection.insert(pools, next);
- 				    });
- 				});
-
- 			}) 
-     	})
+                let tmpAddArr = pool.fullAddress.split(" ");
+                pool.address.postalCode = tmpAddArr[tmpAddArr.length -1];
+                return pool;
+            });
+        }, pools, 0, 1).then(pools => {
+            let imgPromises = pools.map(pool => {
+                pool.tmpName = pool.name.replace(/ /g, "_").toLowerCase();
+                let img_path = path.join(__dirname, `assets/pools/${pool.tmpName}.jpg`);
+                try {
+                    let data = fs.readFileSync(img_path);
+                    return s3.upload(`pools/${pool.tmpName}`, data, 'image/jpg');
+                } catch (err) {
+                    // Ignore image errors
+                    return Promise.resolve();
+                }
+            });
+            return Promise.all(imgPromises).then(imgs => {
+                //append img url
+                for (let i = 0; i < pools.length; i++) {
+                    if (imgs[i] !== undefined) {
+                        pools[i].image = {
+                            url: imgs[i].Location,
+                            verified: true
+                        };
+                    }
+                }
+                //append amenities
+                pools.map(pool => {
+                    pool.amenities = [];
+                    let now = new Date();
+                    pool.createdAt = now;
+                    pool.updatedAt = now;
+                    if (pool.chair != '') {
+                        pool.amenities.push({
+                            type: 'chair',
+                            count: pool.chair,
+                            createdAt: now,
+                            updatedAt: now
+                        });
+                    }
+                    if (pool.daybed != '') {
+                        pool.amenities.push({
+                            type: 'daybed',
+                            count: pool.daybed,
+                            createdAt: now,
+                            updatedAt: now
+                        })
+                    }
+                    if (pool.cabana != '') {
+                        pool.amenities.push({
+                            type: 'cabana',
+                            count: pool.cabana,
+                            createdAt: now,
+                            updatedAt: now
+                        })
+                    }
+                    delete pool.chair;
+                    delete pool.daybed;
+                    delete pool.cabana;
+                    delete pool.tmpName;
+                    delete pool.fullAddress;
+                });
+                return connectionPromise.then(connection => {
+                    connection.db.collection('pools', (error, collection) => {
+                        collection.insert(pools, next);
+                    });
+                });
+            });
+        })
     });
 };
 
 function continuing(fn, array, pos, step, data) {
-	if (!data) data = [];
-	if (pos >= array.length) return Promise.resolve(data);
-	var sub_arr = array.slice(pos, pos + step);
-	var promises = sub_arr.map(fn);
-	return Promise.all(promises).then(current_data => {
-		return continuing(fn, array, pos + step, step, data.concat.apply(data, current_data));
-	}).catch(err => {
-		return Promise.reject(err);
-	});
+    if (!data) data = [];
+    if (pos >= array.length) return Promise.resolve(data);
+    var sub_arr = array.slice(pos, pos + step);
+    var promises = sub_arr.map(fn);
+    return Promise.all(promises).then(current_data => {
+        return continuing(fn, array, pos + step, step, data.concat.apply(data, current_data));
+    }).catch(err => {
+        return Promise.reject(err);
+    });
 };
 
 exports.down = function(next) {
-	connectionPromise.then(connection => {
-	    connection.db.collection('pools', (error, collection) => {
-	        collection.remove({}, next);
-	    });
-	});
+    connectionPromise.then(connection => {
+        connection.db.collection('pools', (error, collection) => {
+            collection.remove({}, next);
+        });
+    });
 };
