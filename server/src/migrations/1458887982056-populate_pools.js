4a9089eae9b6047d51ab5dfe12a230421d1c2345
Nam Chu <namcv910@gmail.com>
2016-03-28 10:48:15 +0700

    remove jsonfile package

--- server/src/migrations/1458887982056-populate_pools.js
+++ server/src/migrations/1458887982056-populate_pools.js
@@ -18,7 +18,6 @@ var httpAdapter = 'https';
 
 const geocoder = require('node-geocoder')(geocoderProvider, httpAdapter);
 //sleep(1000);
-const jsonfile = require('jsonfile');
 exports.up = function(next) {
     let file_path = path.join(__dirname, `assets/pools.json`);
     fs.readFile(file_path, 'utf8', function (err, data) {
