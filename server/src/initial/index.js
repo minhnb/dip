548328475be9bb0b82753e8866ceb772925f0c92
Nam Chu <namcv910@gmail.com>
2016-04-11 13:06:51 +0700

    remove unused files (#106)

--- server/src/initial/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function dipChat() {
-    return process.env.NODE_ENV == 'production' ? '56e7de25e83767f84a923e79': '56e7de25e83767f84a923e79';
-}
-
-module.exports = {
-    dipChat: dipChat
-};
