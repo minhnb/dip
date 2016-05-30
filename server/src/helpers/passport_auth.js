7f0669ead80589ac111848283a263c2684d01604
dttvinh <dtt.vinh@gmail.com>
2016-05-30 13:38:25 +0700

    Fix fblogin api. (#142)

--- server/src/helpers/passport_auth.js
+++ server/src/helpers/passport_auth.js
@@ -90,6 +90,7 @@ function facebookLogin() {
         let hash = crypto.createHmac('sha256', config.facebook.secretId).update(code).digest('hex');
         let request_user_info_url = "https://graph.facebook.com/me?access_token=" + code + "&appsecret_proof=" + hash;
         return request(request_user_info_url).then(fbUserInfo => {
+            fbUserInfo = JSON.parse(fbUserInfo);
             if(!fbUserInfo.email && !ctx.request.body.email) ctx.throw(400, 'Missing Email');
             let email = ctx.request.body.email.toLowerCase();
             return users.findByEmail(fbUserInfo.email).exec().then(user => {
