00e412af27830ea6e18a990f76795150333b20e0
Nam Chu <namcv910@gmail.com>
2016-06-03 14:58:40 +0700

    retrieve updatedAt value

--- server/src/entities/group.js
+++ server/src/entities/group.js
@@ -12,6 +12,7 @@ function convertGroup(group) {
         image: group.image.url,
         members: group.members.map(m => userRef(m.ref)),
         createdAt: group.createdAt,
+        updatedAt: group.updatedAt,
         seen: !group.lastMessage || (currentMember && group.lastMessage._id.equals(currentMember.lastMessage)),
         lastMessage: group.lastMessage.content
     };
@@ -24,6 +25,7 @@ convertGroup.reference = function(group) {
         name: group.name,
         image: group.image.url,
         createdAt: group.createdAt,
+        updatedAt: group.updatedAt,
         seen: !group.lastMessage || (currentMember && group.lastMessage._id.equals(currentMember.lastMessage)),
         lastMessage: group.lastMessage.content
     };
