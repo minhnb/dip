d103d902a2ddb3974c82ea546f99f9f9f76042f0
Nam Chu <namcv910@gmail.com>
2016-06-07 13:56:10 +0700

    revert message.handle

--- server/src/routes/groups/message.handler.js
+++ server/src/routes/groups/message.handler.js
@@ -76,7 +76,8 @@ exports.getMessages = ctx => {
         conditions = {};
 
     conditions['group'] = group;
-    if(from) conditions['updatedAt'] = {$gte: new Date(from).toISOString()};
+    if(from) conditions['createdAt'] = {$gte: new Date(from).toISOString()};
+    
     return db.messages.find(conditions)
         .sort({createdAt: -1})
         .limit(limit)
