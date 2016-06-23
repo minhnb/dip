aa625c0ab248c0ba0320eeb89105f23aead306b1
Nguyen Binh Minh <minhnb89@gmail.com>
2016-06-23 11:14:40 +0700

    Migrate data (#196)
    Update dip event time: 12pm - 6pm

---
+++ server/src/migrations/1466652723168-update-dip-launch-event.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const dotenv = require('dotenv');
+const path = require('path');
+const db = require('../db');
+
+const rootFolder = path.normalize(__dirname + '/../../..');
+
+dotenv.load({
+    path: `${rootFolder}/.env`
+});
+
+exports.up = function(next) {
+    var duration = {
+        "startTime" : 720,
+        "endTime" : 1080
+    };
+    db.events.update({"title": "Launch Dip"}, {$set: {duration: duration}}, (error, result) => {
+        if (error) return next(error);
+        if (result.nModified < 1) {
+            return next("Not Found Dip Event");
+        }
+        return next();
+    });
+};
+
+exports.down = function (next) {
+    next();
+};
