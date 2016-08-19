ccd1dc6850c810578aeba4b1a40db4803c70128e
dttvinh <dtt.vinh@gmail.com>
2016-08-19 17:03:07 +0700

    Change img lib (#326)
    * Change image library.
    
    * Lock down libraries version.
    
    * Update gm lib.

--- server/src/helpers/image.js
+++ server/src/helpers/image.js
@@ -1,21 +1,34 @@
 'use strict';
 
-const imagemagick = require('imagemagick');
+// const imagemagick = require('imagemagick');
+
+// Use imageMagick for jpg/png support
+const gm = require('gm').subClass({'imageMagick': true});
 
 exports.resize = function(buffer, width, format) {
     format = format || 'jpg';
     return new Promise((resolve, reject) => {
-        imagemagick.resize({
-            srcData: buffer,
-            width: width,
-            format: format
-            // maxBuffer: 20*1024*1024
-        }, function (err, stdout, stderr) {
+        var pipe = gm(buffer).setFormat(format);
+        if (width) {
+            pipe = pipe.resize(width);
+        }
+        pipe.toBuffer(function (err, buffer) {
             if (err) reject(err);
-            else {
-                let data = new Buffer(stdout, 'binary');
-                resolve(data);
-            }
+            else resolve(buffer);
         });
+
+
+        // imagemagick.resize({
+        //     srcData: buffer,
+        //     width: width,
+        //     format: format
+        //     // maxBuffer: 20*1024*1024
+        // }, function (err, stdout, stderr) {
+        //     if (err) reject(err);
+        //     else {
+        //         let data = new Buffer(stdout, 'binary');
+        //         resolve(data);
+        //     }
+        // });
     });
 };
